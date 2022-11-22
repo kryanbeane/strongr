a4ba54621bbd3c5034b460761f21fca6d4aae9fa
Bryan Keane <bkeane@redhat.com>
2022-11-22 16:28:59 +0000

    feat: removed google auth due to bugs and implemented email sign in

--- app/src/main/java/com/strongr/activities/LoginActivity.kt
+++ app/src/main/java/com/strongr/activities/LoginActivity.kt
@@ -1,112 +1,105 @@
 package com.strongr.activities
 
-import android.app.Activity
 import android.content.Intent
-import android.content.IntentSender
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.github.ajalt.timberkt.Timber
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
-import com.google.android.gms.auth.api.identity.Identity
-import com.google.android.gms.auth.api.identity.SignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import com.strongr.BuildConfig
-import com.strongr.R
+import com.google.firebase.ktx.initialize
 import com.strongr.databinding.ActivityLoginBinding
+import timber.log.Timber.i
 
 class LoginActivity: AppCompatActivity() {
-    private lateinit var oneTapClient: SignInClient
-    private lateinit var signInRequest: BeginSignInRequest
-    private lateinit var signUpRequest: BeginSignInRequest
+    // Used to bind this logic to the layout - activity_trainee.xml
     private lateinit var binding: ActivityLoginBinding
     private lateinit var auth: FirebaseAuth
-    private lateinit var googleSignInClient: GoogleSignInClient
-    private companion object {
-        private const val TAG = "LoginActivity"
-        private const val RC_SIGN_IN = 9001
-        private const val RC_ONE_TAP = 9002
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        Firebase.initialize(this)
         binding = ActivityLoginBinding.inflate(layoutInflater)
-
-        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(BuildConfig.API_KEY)
-            .requestEmail()
-            .build()
-        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
-
-        // Initialize firebase auth
+        setContentView(binding.root)
         auth = Firebase.auth
-        checkUser()
 
-        // Button click to start
-        binding.logo.setOnClickListener {
-            Toast.makeText(this, "Signing in...", Toast.LENGTH_SHORT).show()
-            val intent = googleSignInClient.signInIntent
-            startActivityForResult(intent, RC_SIGN_IN)
+        Timber.plant(Timber.DebugTree())
+        i("Trainee Activity Started Successfully...")
+
+
+
+        binding.signUp.setOnClickListener() {
+            signUp(binding.emailAddress.text.toString(), binding.password.text.toString())
+        }
+
+        binding.signIn.setOnClickListener() {
+            signIn(binding.emailAddress.text.toString(), binding.password.text.toString())
         }
     }
 
-
-    private fun checkUser() {
-
-    }
-
-    @Deprecated("Deprecated in Java")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if(requestCode == RC_SIGN_IN) {
-            Toast.makeText(this, "Signing in...", Toast.LENGTH_SHORT).show()
-            val accountTask = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-                val account = accountTask.getResult(Exception::class.java)
-                firebaseAuthWithGoogleAccount(account)
-            } catch (e: Exception) {
-                Toast.makeText(this, "Error: ${e.message}", Toast.LENGTH_SHORT).show()
-            }
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            // Open workout activity
+            val intent = Intent(this, WorkoutActivity::class.java)
+            startActivity(intent)
         }
     }
 
-    private fun firebaseAuthWithGoogleAccount(account: GoogleSignInAccount?) {
-        Toast.makeText(this, "Signing in...", Toast.LENGTH_SHORT).show()
-
-        val credentials = GoogleAuthProvider.getCredential(account?.idToken, null)
-        auth.signInWithCredential(credentials)
-            .addOnSuccessListener { authResult ->
-                Toast.makeText(this, "Signed in successfully", Toast.LENGTH_SHORT).show()
-                val firebaseUser = auth.currentUser
-                val uid = firebaseUser?.uid
-                val email = firebaseUser?.email
-                Toast.makeText(this, "User: $uid, $email", Toast.LENGTH_SHORT).show()
-
-                if (authResult.additionalUserInfo!!.isNewUser) {
-                    // User is new, show them a fancy intro screen!
-                    Toast.makeText(this@LoginActivity, "New user", Toast.LENGTH_SHORT).show()
-                } else {
-                    // User is old, show them a welcome back screen.
-                    Toast.makeText(this, "Old user", Toast.LENGTH_SHORT).show()
+    private fun signIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { result ->
+                if (result.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    i("Welcome to Strongr ${auth.currentUser?.email}")
+                    val user = auth.currentUser
+                    val switchActivityIntent = Intent(this, WorkoutActivity::class.java)
+                    startActivity(switchActivityIntent)
                 }
-
+                else {
+                    Toast.makeText(this, result.exception!!.message, Toast.LENGTH_SHORT).show()
+                }
             }
-            .addOnFailureListener { e ->
-                Toast.makeText(this, "Error: ${e.message}", Toast.LENGTH_SHORT).show()
-            }
-
     }
+
+    private fun signUp(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { result ->
+                if (result.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    i("Welcome to Strongr ${auth.currentUser?.email}")
+                    val user = auth.currentUser
+                    val switchActivityIntent = Intent(this, WorkoutActivity::class.java)
+                    startActivity(switchActivityIntent)
+                }
+                else {
+                    when (result.exception!!.javaClass) {
+                        // If the user enters an email address that is already in use
+                        FirebaseAuthUserCollisionException::class.java -> {
+                            Toast.makeText(this, "User already exists, try singing in!", Toast.LENGTH_SHORT).show()
+                        }
+                        // If the user enters a weak password
+                        FirebaseAuthWeakPasswordException::class.java -> {
+                            Toast.makeText(this, "Password is too weak, try a stronger password!", Toast.LENGTH_SHORT).show()
+                        }
+                        // If the user enters an invalid email address
+                        FirebaseAuthInvalidCredentialsException::class.java -> {
+                            Toast.makeText(this, "Invalid email address, try again!", Toast.LENGTH_SHORT).show()
+                        }
+                        // Some other arbitrary exception
+                        else -> {
+                            Toast.makeText(this, result.exception!!.message, Toast.LENGTH_SHORT).show()
+                            i(result.exception)
+                        }
+                    }
+                }
+            }
+    }
+
 
 }
-
